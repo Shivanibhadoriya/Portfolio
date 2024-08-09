9ab04f81b1adca417f9d14597f502a10e629649a
Shivanibhadoriya <shivanibhadouriya2003@gmail.com>
2024-08-09 22:08:25 +0530

    add projects

--- script.js
+++ script.js
@@ -18,3 +18,34 @@ document.querySelector(".navbar-toggler").addEventListener("click",function(){
     console.log("I am clicked");
     document.querySelector(".collapse").style.display="block !important";
 });
+
+const projects = document.querySelectorAll(".project");
+projects.forEach(project =>{
+  console.log("focus check");
+  project.addEventListener('mousedown',function(){
+   console.log("focus gain");
+    let images = project.querySelectorAll(".project-img");
+    let i = 0; 
+    let size = images.length;
+    for(i=0;i<size ;i++){
+        if(images[i].classList.contains("active"))
+            break;
+    }
+    let leftButton = project.querySelector(".left");
+    leftButton.addEventListener("click",function(){
+        console.log("i am left click");
+        images[i].classList.remove("active");
+        i = (i+size-1)%size;    
+        images[i].classList.add("active");
+    });
+
+    let rightButton = project.querySelector(".right");
+    rightButton.addEventListener("click",function(){
+        console.log("i am right click");
+        images[i].classList.remove("active");
+        i = (i+size+1)%size;    
+        images[i].classList.add("active");
+    });
+});
+  
+});
